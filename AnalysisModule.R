fea4d277acf80a1e991827e414c5d18fb391d05a
power1588 <power15@126.com>
2015-10-06 21:51:11 +1300

    part of explaination

---
+++ AnalysisModule.R
@@ -0,0 +1,23 @@
+###############################
+#I want to build up my first
+#paper program in RStudio with Reproducible
+#Research (RR) method,i.e gathering data
+#analysing data, writing paper,and 
+#preseting beamer all together by different
+#modules.
+###############################
+#Analysing Module
+#Purpose: Using VAR to analyse Index futures basis and
+#stock liqudity, consider the shorting ban in China
+#examine the relationship between liquidity and 3 types of
+#index futures basis(IC/IF/IH) 
+#Using Method:
+#Begin Date:
+#Coder:
+#
+###############################
+
+
+
+
+
