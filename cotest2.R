dfddd4c94d240321bb04fd0c9e7d5a70c68f5fc2
power1588 <power15wy@gmail.com>
2016-01-16 16:55:53 +1300

    Add Hasbrouk Information ratio to find out the pricing efficiency

--- cotest2.R
+++ cotest2.R
@@ -1,6 +1,10 @@
 
 #specially for IF with 3 dummy variables 
+
+
 tid <- rep(18:1, rep(240,18))
+N <- ((length(if00c)-length(tid))%/%240)
+M <- N%/%20
 tid0 <- rep(20:1, rep(240,20))
 tidt <- rep(20:12, rep(240,9))
 tidz <- rep(tid0, M)
@@ -16,7 +20,7 @@ cotest2 <- function(s,f,n=1){
   y3 <- 174480
   #1.test cointegrate between spot and futures and get the residuals as
   #the error correction term
-  lmresult <- lm(f ~ s+ tid)
+  lmresult <- lm(f ~ s)
   #lmresult <- lm(f ~ s)
   ect <- lmresult$residuals
   #2. use the ect(erro correction term) and lag term of spot and futures
@@ -42,16 +46,52 @@ cotest2 <- function(s,f,n=1){
   cotest4 <- lm(s1 ~ dum4[-c(1:3)]:ect[-c(1:3)] +dum5[-c(1:3)]:ect[-c(1:3)]+  s2 + s3 + f2 + f3)
   cotest2 <- lm(f1 ~  dum1[-c(1:3)]:ect[-c(1:3)] + dum2[-c(1:3)]:ect[-c(1:3)]+dum3[-c(1:3)]:ect[-c(1:3)]  
                 +  s2 + s3 + f2 + f3)
+  e1 <- cotest1$residuals
+  e2 <- cotest2$residuals
+  v1 <- sd(e1)
+  v2 <- sd(e2)
+  rho <- cor(e1,e2)
+  d11 <- cotest1$coefficients[6]
+  names(d11) <- NULL
+  d12 <- cotest1$coefficients[7]
+  names(d12) <- NULL
+  d13 <- cotest1$coefficients[8]
+  names(d13) <- NULL
+  d21 <- cotest2$coefficients[6]
+  names(d21) <- NULL
+  d22 <- cotest2$coefficients[7]
+  names(d22) <- NULL
+  d23 <- cotest2$coefficients[8]
+  names(d23) <- NULL
+  
+  #H11 <- (d11*v1 + d21*rho*v2)^2/(((d11*v1 + d21*rho*v2)^2)+(d21*v2*sqrt(1-rho^2))^2)
+  #H12 <- (d21*v2*sqrt(1-rho^2))^2/(((d11*v1 + d21*rho*v2)^2)+(d21*v2*sqrt(1-rho^2))^2)
+  H11 <- (-d21*v1 + d11*rho*v2)^2/(((d21*v1 + d11*rho*v2)^2)+(d11*v2*sqrt(1-rho^2))^2)
+  H12 <- (d11*v2*sqrt(1-rho^2))^2/(((d21*v1 + d11*rho*v2)^2)+(d11*v2*sqrt(1-rho^2))^2)
+  
+  
+  #H21 <- (d12*v1 + d22*rho*v2)^2/(((d12*v1 + d22*rho*v2)^2)+(d22*v2*sqrt(1-rho^2))^2)
+  #H22 <- (d22*v2*sqrt(1-rho^2))^2/(((d12*v1 + d22*rho*v2)^2)+(d22*v2*sqrt(1-rho^2))^2)
+  
+  H21 <- (-d22*v1 + d12*rho*v2)^2/((((-d22*v1 + d12*rho*v2)^2)+(d12*v2*sqrt(1-rho^2))^2))
+  H22 <- (d12*v2*sqrt(1-rho^2))^2/((((-d22*v1 + d12*rho*v2)^2)+(d12*v2*sqrt(1-rho^2))^2))
+  
+  H31 <- (-d23*v1 + d13*rho*v2)^2/(((d23*v1 + d13*rho*v2)^2)+(d13*v2*sqrt(1-rho^2))^2)
+  H32 <- (d13*v2*sqrt(1-rho^2))^2/((((-d23*v1 + d13*rho*v2)^2)+(d13*v2*sqrt(1-rho^2))^2))
+  
+  
+  
   # regression on log(futures) ~~ ect + dum1*ect + dum2*ect +diff(lag(s)) + diff(lag(f))
   #cotest2 <- lm(diff(f) ~ ect[-1] + dum1:ect[-1] + dum2:ect[-1]  
   #              +  diff(lag(s)) + diff(lag(f)) )
-  if (n ==1){return(summary(cotest1))}
+  #if (n ==1){return(summary(cotest1))}
+  return(c((H11+H12)/2,(H21+H22)/2,(H31+H32)/2))
   if (n ==2){return(summary(cotest2))}
   if (n ==3){return(summary(cotestspec))}
   if (n ==4){return(summary(cotest4))}
 }
 
 cotest2(hs300c,if00c,1)
-cotest2(hs300c,if00c,2)
-cotest2(hs300c,if00c,3)
-cotest2(hs300c,if00c,4)
+#cotest2(hs300c,if00c,2)
+#cotest2(hs300c,if00c,3)
+#cotest2(hs300c,if00c,4)
